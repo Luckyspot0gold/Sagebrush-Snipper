abe907097e37311e11982f8cd16fd781aa09c602
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-19 15:20:50 -0600

    Create revinue_streams.js

---
+++ footer/revinue_streams.js
@@ -0,0 +1,10 @@
+<div class="revenue-modules">
+  <div class="revenue-card">
+    <h4>NFT Porting Service</h4>
+    <button id="fiverr-connect">Earn $200/Port</button>
+  </div>
+  <div class="revenue-card">
+    <h4>Battle Commission</h4>
+    <div class="royalty-display">7.5% Royalty Active</div>
+  </div>
+</div>
